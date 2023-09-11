8527e3a2ba69424849f4b9c439188d42fef9ebad
Abdulmuqadam <79280932+Abdulmuqadam@users.noreply.github.com>
2023-09-11 18:22:33 +0500

    code first approch ef

---
+++ CRUD Implementation with ORM with Code First Approch/CodeFirstApproch/Models/Products.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeFirstApproch.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Manufacture { get; set; }
+        public decimal Price { get; set; }
+        public decimal DateOfExpire { get; set; }
+        public decimal DateofMake { get; set; }
+    }
+}
