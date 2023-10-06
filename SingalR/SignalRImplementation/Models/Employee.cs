f9bb028d8c257a2984fd88d54274c9b01c79a737
Abdulmuqadam <79280932+Abdulmuqadam@users.noreply.github.com>
2023-10-06 19:35:39 +0500

    complete

---
+++ SingalR/SignalRImplementation/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalRImplementation.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }                
+
+    }
+}
---
+++ SingalR/SignalRImplementation/Utilities/RealTimeHub.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRImplementation.Utilities
+{
+    public class RealTimeHub : Hub
+    {
+        public async Task SendRealTimeData(string data)
+        {
+            try
+            {
+                if (data != null)
+                {
+                    if (Clients != null)
+                    {
+                        if (Clients.All != null)
+                        {
+                            await Clients.All.SendAsync("ReceiveRealTimeData", data);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Clients.All is Null");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Client is null");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("data is null");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log any exceptions
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
